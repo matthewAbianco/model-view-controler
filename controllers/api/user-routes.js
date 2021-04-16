437a06384584ce7d8e01837274bfc5ee66fea5d1
Matt Bianco <matthewbianco@protonmail.com>
2021-04-16 18:56:37 -0400

    updated routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-});
+ });
 
 router.get('/:id', (req, res) => {
     User.findOne({
