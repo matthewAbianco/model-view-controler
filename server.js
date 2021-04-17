257152a010c06e4c6364a1d3c9c3b7ede69b4e7d
Matt Bianco <matthewbianco@protonmail.com>
2021-04-16 20:17:30 -0400

    testing seeds

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () =>{seeds(); console.log('Now listening')});
 });
